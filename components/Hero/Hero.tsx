0d25dbad031583d07c2b0789ac69c9121264b8aa
Jonatas-01 <jonatasmc02@gmail.com>
2025-12-07 19:05:04 +0000

    style: Estilização do título no Hero

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 export default function Hero() {
     return (
         <div className="flex flex-col mt-10 md:mt-15">
-            <div className="mx-auto w-full px-4 text-center md:w-1/2">
-                <h1 className="text-4xl font-bold">Soluções Completa para  Escritório e Escolares</h1>
+            <div className="mx-auto w-full px-4 text-center md:max-w-[600px]">
+                <h1 className="primary-title">Soluções Completa para  Escritório e Escolares</h1>
                 <p className="mt-3">Sempre  alinhados às necessidades de escolas, empresas e instituições que buscam ambientes mais confortáveis, eficientes e modernos.</p>
             </div>
             <div className="mt-3 px-4 md:px-0 relative">
