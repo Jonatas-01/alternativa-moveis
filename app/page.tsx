6d848d12c70a6d6419151f62b6928d6f0f04483b
Jonatas-01 <jonatasmc02@gmail.com>
2025-12-07 20:15:11 +0000

    feat: Criação do componente Servicos

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Hero from '@/components/Hero/Hero';
+import Servicos from '@/components/Servicos/Servicos';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <div className="container mx-auto px-6">
+        <Servicos />
+      </div>
     </main>
   );
 }
--- components/Servicos/Servicos.tsx
+++ components/Servicos/Servicos.tsx
@@ -1,8 +1,44 @@
+import { PiOfficeChairFill } from "react-icons/pi";
+import { BiSolidSchool } from "react-icons/bi";
+import { BsTools } from "react-icons/bs";
+import { TbTruckDelivery } from "react-icons/tb";
 
 export default function Servicos() {
     return (
-        <div>
-            
+        <div className="mt-8">
+            <div className="mb-4">
+                <h1 className="secondary-title">Nossos Serviços</h1>
+            </div>
+            <div className="flex justify-center flex-wrap gap-10 [&>*]:max-w-[340px]">
+                <div className="text-center flex flex-col items-center servico-card">
+                    <PiOfficeChairFill className="justify-center mb-2" size={70} color="var(--secondary)" />
+                    <div>
+                        <h4 className="text-xl mb-2 font-semibold">Venda de Móveis Novos</h4>
+                        <p className="text-sm pb-3">Qualidade, design e variedade para equipar sua empresa</p>
+                    </div>
+                </div>
+                <div className="text-center flex flex-col items-center servico-card">
+                    <BsTools className="justify-center mb-2" size={70} color="var(--secondary)" />
+                    <div>
+                        <h4 className="text-xl mb-2 font-semibold">Conserto Especializado</h4>
+                        <p className="text-sm pb-3">Aumente a durabilidade dos seus ativos com nossa expersite técnica</p>
+                    </div>
+                </div>
+                <div className="text-center flex flex-col items-center servico-card">
+                    <TbTruckDelivery className="justify-center mb-2" size={70} color="var(--secondary)" />
+                    <div>
+                        <h4 className="text-xl mb-2 font-semibold">Entrega á Domicilio</h4>
+                        <p className="text-sm pb-3">Entrega garatida dos produtos em sua porta</p>
+                    </div>
+                </div>
+                <div className="text-center flex flex-col items-center servico-card">
+                    <BiSolidSchool className="justify-center mb-2" size={70} color="var(--secondary)" />
+                    <div>
+                        <h4 className="text-xl mb-2 font-semibold">Trabalhos  Escolares</h4>
+                        <p className="text-sm pb-3">Oferecemos móveis e restaurações em ambientes escolares</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
