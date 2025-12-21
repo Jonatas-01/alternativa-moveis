30eb369bf57559b09568997028905804f7584835
Jonatas-01 <jonatasmc02@gmail.com>
2025-12-21 17:31:16 +0000

    style: Máximo de 2 linhas na descrição do produto no carrosel

--- components/Destaque/ProdutoCard.tsx
+++ components/Destaque/ProdutoCard.tsx
@@ -10,14 +10,14 @@ type ProdutoCardProps = {
 
 export default function ProdutoCard({ foto, categoria, nome, descricao, preco }: ProdutoCardProps) {
     return (
-        <div>
+        <div className="">
             <div className="overflow-hidden">
                 <Image src={foto} alt={nome} width={440} height={440} className="object-cover max-h-[265px] max-w-[265px] sm:max-h-[440px] sm:max-w-[440px]" />
             </div>
-            <div className="mt-2 max-h-[265px] max-w-[265px] sm:max-h-[440px] sm:max-w-[440px]">
+            <div className="p-2 max-h-[265px] max-w-[265px] sm:max-h-[440px] sm:max-w-[440px]">
                 <p className="font-medium ">{categoria}</p>
                 <h4 className="font-semibold text-lg">{nome}</h4>
-                <p className="">{descricao}</p>
+                <p className="line-clamp-2">{descricao}</p>
                 <h4 className="font-semibold text-lg">R$ {preco.toFixed(2)}</h4>
             </div>
         </div>
