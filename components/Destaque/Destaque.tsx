733845d949cc4be704227ec3f0c1ecbaee65ec36
Jonatas-01 <jonatasmc02@gmail.com>
2025-12-21 18:15:01 +0000

    feat: Mostra produtos em destaque somente se houver 4 ou mais adicionados pelo admin

--- components/Destaque/Destaque.tsx
+++ components/Destaque/Destaque.tsx
@@ -1,21 +1,57 @@
+'use client'
 
 import { BsArrowRight } from "react-icons/bs";
 import EmblaCarousel from "../ui/EmblaCarousel"
 import { EmblaOptionsType } from 'embla-carousel'
+import { supabase } from '@/lib/supabase-client'
+import React, { useState } from 'react'
 
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
 const SLIDE_COUNT = 4
 const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
+interface Product {
+    id: string;
+}
+
 export default function Destaque() {
+    const [featuredProducts, setFeaturedProducts] = useState<Product[]>([])
+
+    const fetchFeaturedProducts = async () => {
+        const { data, error } = await supabase
+            .from('products')
+            .select('*')
+            .eq('is_featured', true)
+
+        if (error) {
+            console.error('Erro ao buscar produtos em destaque:', error.message)
+            return []
+        }
+
+        return data || []
+    }
+
+    React.useEffect(() => {
+        const loadFeaturedProducts = async () => {
+            const products = await fetchFeaturedProducts()
+            setFeaturedProducts(products)
+        }
+
+        loadFeaturedProducts()
+    }, [])
+
     return (
         <div className="mt-14">
-            <div className="container mx-auto px-6">
-                <h1 className="secondary-title">Produtos em Destaque</h1>
-            </div>
-            <div className="mt-7">
-                <EmblaCarousel slides={SLIDES} options={OPTIONS} />
-            </div>
+            {featuredProducts.length >= 4 && (
+                <>
+                    <div className="container mx-auto px-6">
+                        <h1 className="secondary-title">Produtos em Destaque</h1>
+                    </div>
+                    <div className="mt-7">
+                        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+                    </div>
+                </>
+            )}
             <div className="flex justify-center mt-5">
                 <a href="/catalogo" className="flex items-center botoes gap-3">Ver Catálogo <BsArrowRight /></a>
             </div>
